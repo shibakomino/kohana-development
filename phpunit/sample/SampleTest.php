8fc027a4535ff6202bd1125eaee2c806fc9f59d2
Colin Leung <colin@digi3studio.com>
2017-09-05 15:00:36 +0800

    add unit test use namespaced kohana core.

---
+++ phpunit/sample/SampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class SampleTest extends PHPUnit_Framework_TestCase
+{
+    function setUp()
+    {
+
+    }
+
+    public function testInstall()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function tearDown()
+    {
+
+    }
+}
